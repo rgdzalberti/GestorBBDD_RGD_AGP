df28def8a945d631bbe960d24292b2c8c1bb5196
agpalberti <agonzalezp@iesrafaelalberti.es>
2022-12-07 17:03:31 +0100

    Cambios en alguna documentación

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,19 +5,20 @@ import vista.GUI
 import vista.UserInterface
 
 /**
-* Hola Diego.
+ * Hola Diego. Para facilitarte la lectura de la documentación, te recomendamos que pulses Ctrl + Alt + Q, que activará el modo lectura, y
+ * formateará el texto documentado y lo hará mucho más agradable a la vista :).
  * Comentarios:
  *
- * En [Controlador], en el init, se llama a la función [GestorBDD.createTablePeliculas], que no debería ser necesario si utilizas una base de datos normal,
- * pero nosotros utilizamos una que se guarda solo en tiempo de ejecución, por lo que con cada ejecución del main se borra la tabla Peliculas.
+ * - En [Controlador], en el init, se llama a la función [GestorBDD.createTablePeliculas], que no debería ser necesario si utilizas una base de datos normal,
+ * pero nosotros utilizamos una que se guarda solo en tiempo de ejecución, por lo que con cada ejecución del main se borra la tabla Películas.
  * Por eso, si vas a cambiar la base de datos para probar la aplicación, para crear la tabla la puedes crear mediante la consola o mediante la primera ejecución
  * del programa, pero después de eso no sé si dará error al intentar crear una tabla ya existente, por lo que recomiendo que borres la línea 19 de [Controlador]
  * Para más información, haz Ctrl + Click Izquierdo aquí: [GestorBDD.createTablePeliculas]
  *
- * Los textos que se van a imprimir por pantalla los hemos dividido en una clase aparte, que se encuentra en [vista.strings.Strings],
+ * - Los textos que se van a imprimir por pantalla los hemos dividido en una clase aparte, que se encuentra en [vista.strings.Strings],
  * y son los que se usan en [GUI].
  *
- * La [vista] la hemos dividido en una clase [GUI] y una interfaz [UserInterface], para que en vez de insertar la vista [GUI] directamente a el [Controlador],
+ * - La [vista] la hemos dividido en una clase [GUI] y una interfaz [UserInterface], para que en vez de insertar la vista [GUI] directamente a el [Controlador],
  * puedas insertar la que quieras mientras implemente [UserInterface], o lo que es lo mismo, hemos implementado el patrón de diseño de injección de dependencias.
  *
  * @author Alejandro González Parra y Ricardo Gallego Domínguez
